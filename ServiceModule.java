5db5fdae674fd53b174cacd5e824ebc7eb36535c
Gautam Sethia <gomcisethia@gmail.com>
2022-11-18 20:47:13 +0530

    output format changed

--- ServiceModule.java
+++ ServiceModule.java
@@ -25,7 +25,8 @@ class QueryRunner implements Runnable
    public void run()
    {
      try{        
-            this.con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE);  
+            this.con.setTransactionIsolation(Connection.TRANSACTION_SERIALIZABLE); 
+            this.con.setAutoCommit(false); 
             // Reading data from client
            InputStreamReader inputStream = new InputStreamReader(socketConnection
                                                        .getInputStream());
@@ -37,6 +38,7 @@ class QueryRunner implements Runnable
            String responseQuery = "" ;
            String queryInput = "" ;
            String inputQ="";
+           int query=1;
            while(true)
            {
                // Read client query
@@ -57,21 +59,40 @@ class QueryRunner implements Runnable
                    socketConnection.close();
                    return;
                }
+               String delimSpace = "[ ]+";
+               String[] sample  = clientCommand.split(delimSpace);
+               len =sample.length;
                if(len==4)
                {
-                   String delimSpace = "[ ]+";
+                   delimSpace = "[ ]+";
                    String[] arr1  = clientCommand.split(delimSpace);
                    inputQ=inputQ+"SELECT * from insert_train(";
                    inputQ=inputQ+arr1[0]+",";
-                   inputQ=inputQ+arr1[3]+",";
                    inputQ=inputQ+arr1[2]+",";
+                   inputQ=inputQ+arr1[3]+",";
                    inputQ=inputQ+"'"+arr1[1]+"');";
                 //    System.out.println(inputQ);
                    try{
-                        this.con.setAutoCommit(false);
+                        
                         try {
                             Statement st = this.con.createStatement();
-                            st.executeQuery(inputQ);
+                            ResultSet rs =st.executeQuery(inputQ);
+                            while(rs.next())
+                            {
+                                System.out.println(rs.getInt(1));
+                                if(rs.getInt(1)==1)
+                                {
+                                    responseQuery = "Train " + arr1[0] + " has been inserted on " + arr1[1];
+                                }
+                                else if(rs.getInt(1)==0)
+                                {
+                                    responseQuery = "Train " + arr1[0] + " is an invalid train";
+                                }
+                                else
+                                {
+                                    responseQuery = "Train " + arr1[0] + " already exists for " + arr1[1];
+                                }
+                            }
                             // System.out.println("query executed");
                             this.con.commit();
                         } catch (SQLException e) {
@@ -84,7 +105,7 @@ class QueryRunner implements Runnable
                     }
                    inputQ="";
                }
-               else if(len<4)
+               else
                {
                 //    System.out.println(pnr);
                    String forfunc ="";
@@ -92,8 +113,8 @@ class QueryRunner implements Runnable
                    String dummy="";
                    forfunc=forfunc+"SELECT * from book_tickets(";
                    inputQ=inputQ+"SELECT * from is_seat_available(";
-                   String delimSpace = "[, ]+";
-                   String[] arr1  = clientCommand.split(delimSpace);
+                   String delim = "[, ]+";
+                   String[] arr1  = clientCommand.split(delim);
                    int x =arr1.length;
                    inputQ=inputQ+arr1[x-3]+",";
                    inputQ=inputQ+"'"+arr1[x-2]+"',";
@@ -111,20 +132,20 @@ class QueryRunner implements Runnable
                    forfunc=forfunc+arr1[x-1].charAt(0)+"',";
  
                    String []output=new String[Integer.parseInt(arr1[0])];
-                   System.out.println(inputQ);
+                //    System.out.println(inputQ);
                    int canBook = -1;
                    int counter = 0;
                    while(counter < max_try){
                         try{
-                            this.con.setAutoCommit(false);
                             Statement st = this.con.createStatement();
                             ResultSet rs = st.executeQuery(inputQ);
+                            this.con.commit();
                             while(rs.next()){
                                 canBook =rs.getInt(1);
-                                System.out.println(rs.getInt(1));
+                                // System.out.println(rs.getInt(1));
                                 forfunc=forfunc+String.valueOf(canBook);
                             }
-                                this.con.commit();
+                               
                                 counter = max_try;
                             }
                             catch(SQLException e){
@@ -135,16 +156,18 @@ class QueryRunner implements Runnable
                     }
                     try{
                         if(canBook>=0){
-                        forfunc=forfunc+",";
-                        forfunc=forfunc+arr1[x-3]+",'"+arr1[x-2]+"');";
-                        System.out.println(forfunc);
-                                Statement st = this.con.createStatement();
-                                ResultSet rs = st.executeQuery(forfunc);
-                                while(rs.next()){
-                                    System.out.println(rs.getString(1));
-                                    dummy=rs.getString(1);
-                                }
-                            String[] arr  = dummy.split(delimSpace);  
+                            forfunc=forfunc+",";
+                            forfunc=forfunc+arr1[x-3]+",'"+arr1[x-2]+"');";
+                            // System.out.println(forfunc);
+                            Statement st = this.con.createStatement();
+                            ResultSet rs = st.executeQuery(forfunc);
+                            this.con.commit();
+                            while(rs.next()){
+                                // System.out.println(rs.getString(1));
+                                dummy=rs.getString(1);
+                            }
+                            // System.out.println("2 " + dummy);
+                            String[] arr  = dummy.split(delim);  
                             int l=arr.length;
                             int in=1;  
                             for(int i=0;i<Integer.parseInt(arr1[0]) && in<l;i++)
@@ -175,24 +198,40 @@ class QueryRunner implements Runnable
                                 }
                             }
 
-                            String heading="";
-                            heading="PNR: " + arr[0] + "\n" + "Train No: "+ arr1[x-3]+"\nDate Of Journey: "+arr1[x-2];
-                            System.out.println(heading); 
+                            String heading="#Query:";
+                            String q=Integer.toString(query);
+                            heading=heading+q+"\n";
+                            // System.out.println(heading);
+                            // System.out.println(query);
+                            heading=heading+"PNR: " + arr[0] + "\n" + "Train No: "+ arr1[x-3]+"\nDate Of Journey: "+arr1[x-2];
+                            // System.out.println(heading);
+                            responseQuery=heading; 
                             for(int i=0;i<Integer.parseInt((arr1[0]));i++)
                             {
-                                System.out.println(output[i]);
-                            }
-                            
+                                responseQuery=responseQuery+"\n   ";
+                                responseQuery=responseQuery+output[i];
+                            }  
+                            responseQuery=responseQuery+"\n\n\n\n";
                         }
                     else{
-                        System.out.println("Seat can not be booked!");
+                        String heading="#Query:";
+                            String q=Integer.toString(query);
+                            heading=heading+q+"\n";
+                            // System.out.println(heading);
+                            // System.out.println(query);
+                            heading=heading+"Train No: "+ arr1[x-3]+"\nDate Of Journey: "+arr1[x-2];
+                            // System.out.println(heading);
+                            responseQuery=heading; 
+                            responseQuery=responseQuery+"\nSeat can not be booked!\n\n\n\n";
+                        // System.out.println("Seat can not be booked!");
                     }
-                    this.con.commit();
+                    // System.out.println("ended");
                 }
                 catch(SQLException e){
                     this.con.rollback();
                     e.printStackTrace();
                 }
+                
             }
                     inputQ="";
                    // for (String uniqVal1 : arr1)
@@ -220,7 +259,7 @@ class QueryRunner implements Runnable
                //     e.printStackTrace();
                // }
  
-               responseQuery = "******* Dummy result ******";
+            //    responseQuery = "******* Dummy result ******";
  
                //----------------------------------------------------------------
               
@@ -228,7 +267,7 @@ class QueryRunner implements Runnable
                printWriter.println(responseQuery);
                // System.out.println("\nSent results to client - "
                //                     + socketConnection.getRemoteSocketAddress().toString() );
-              
+              query=query+1;
            }
        }
        catch(Exception e)
